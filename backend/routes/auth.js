bdd57169d1ed8c415234f8aaf5d51d39da866e2b
Kritik Kapoor <kritik0401@gmail.com>
2022-06-27 18:59:33 +0530

    backend completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchuser");
 
+const JWT_KEY = "MyFirstKey";
+
+//Route 1 Create a user using: POST "/api/auth/createuser". No login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Name must contain atleast 3 characters").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
@@ -12,17 +18,89 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    //return bad request for errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    }).then((user) => res.json(user));
+    //check whether the user already exists
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "This email already exists" });
+      }
+      const salt = await bcrypt.genSaltSync(10);
+      const secPass = await bcrypt.hashSync(req.body.password, salt);
+
+      // create new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, JWT_KEY);
+
+      res.json({ token });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some internal error occured in the code");
+    }
   }
 );
 
+//Route 2 User authentication POST "/api/auth/login". No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Please enter a valid Password").exists(),
+  ],
+  async (req, res) => {
+    //return bad request for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Enter correct credentials" });
+      }
+      const compPass = await bcrypt.compare(password, user.password);
+      if (!compPass) {
+        return res.status(400).json({ error: "Enter correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, JWT_KEY);
+      res.json({ token });
+    } catch (error) {
+      onsole.error(error.message);
+      res.status(500).send("some internal error occured in the code");
+    }
+  }
+);
+
+// Route 3 Get user data using : POST "/api/auth/getuser". login required
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("some internal error occured in the code");
+  }
+});
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,4 +1,110 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
+
+//Route 1 : Fetch all user notes using : GET "/api/notes/fetchallnotes"
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("some internal error occured in the code");
+  }
+});
+
+//Route 2 : Add a note : POST "/api/notes/addnote" : login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Title must contain atleast 3 characters").isLength({
+      min: 3,
+    }),
+    body(
+      "description",
+      "Description must contain atleast 5 characters"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const { title, description, tag } = req.body;
+    //return bad request for errors
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const SavedNote = await note.save();
+      res.json(SavedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some internal error occured in the code");
+    }
+  }
+);
+
+//Route 3 : Update a note : POST "/api/notes/updatenote/:id" : login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  // create a newNote object
+  try {
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not allowed");
+    }
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json({ note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("some internal error occured in the code");
+  }
+});
+
+//Route 4 : Delete an existing note : DELETE "/api/notes/deletenote/:id" : login required
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  try {
+    //Find the note to be deleted and delete it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    //Allow deletion only if user owns this note
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not allowed");
+    }
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been deleted", note: note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("some internal error occured in the code");
+  }
+});
 
 module.exports = router;
